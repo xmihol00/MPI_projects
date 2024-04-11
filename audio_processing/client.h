1d9569d617fa0099ff17593b93bc700a11551c4e
David Mihola <mihola.dejv@gmail.com>
2024-04-11 10:07:06 +0200

    main-Audio processing setup

---
+++ audio_processing/client.h
@@ -0,0 +1,4 @@
+#ifndef _CLIENT_H_
+#define _CLIENT_H_
+
+#endif  // _CLIENT_H_
---
+++ audio_processing/main.cpp
@@ -0,0 +1,9 @@
+#include "mian.h"
+
+int main(int argc, char* argv[])
+{
+    MPI_Init(&argc, &argv);
+
+    MPI_Finalize();
+    return 0;
+}
